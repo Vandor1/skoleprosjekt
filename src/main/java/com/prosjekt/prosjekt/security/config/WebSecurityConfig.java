82ea2c6ec7df5c54545ae35caf61ae05218ed119
Mathias <mvbend@stud.ntnu.no>
2021-04-27 11:36:30 +0200

    bugfix

--- src/main/java/com/prosjekt/prosjekt/security/config/WebSecurityConfig.java
+++ src/main/java/com/prosjekt/prosjekt/security/config/WebSecurityConfig.java
@@ -22,9 +22,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().disable();
-        http.csrf().disable().authorizeRequests()
-                .antMatchers("/api/v*/registration/**").permitAll()
-                .anyRequest().authenticated().and().formLogin().disable();
+        http.csrf().disable().formLogin().disable();
     }
 
     @Override
